318ed73a73a5519b41f6bd14377f01bc99e7ef40
Faisal <falquaddoomi@ucla.edu>
2018-05-04 16:38:32 +0200

    Update AlertProvider.js
    wrapped children in new react 16 context provider

--- app/components/Alert/AlertProvider.js
+++ app/components/Alert/AlertProvider.js
@@ -3,27 +3,23 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import DropdownAlert from 'react-native-dropdownalert';
 
-class AlertProvider extends Component {
-  static childContextTypes = {
-    alertWithType: PropTypes.func,
-    alert: PropTypes.func,
-  };
+import {AlertContext} from './index.js';
 
+class AlertProvider extends Component {
   static propTypes = {
     children: PropTypes.any,
   };
 
-  getChildContext() {
-    return {
-      alert: (...args) => this.dropdown.alert(...args),
-      alertWithType: (...args) => this.dropdown.alertWithType(...args),
-    };
-  }
-
   render() {
     return (
       <View style={{ flex: 1 }}>
-        {React.Children.only(this.props.children)}
+        <AlertContext.Provider value={
+          alert: (...args) => this.dropdown.alert(...args),
+          alertWithType: (...args) => this.dropdown.alertWithType(...args),
+        }>
+          {React.Children.only(this.props.children)}
+        </AlertContext.Provider>
+    
         <DropdownAlert
           ref={(ref) => {
             this.dropdown = ref;
